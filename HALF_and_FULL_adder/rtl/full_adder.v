e29da3daff40f5880334f6ccc9f2beb51ef1f0fc
mishkat29 <49838423+mishkat29@users.noreply.github.com>
2021-07-13 12:17:14 +0530

    Half_and_full_adder

---
+++ HALF_and_FULL_adder/rtl/full_adder.v
@@ -0,0 +1,21 @@
+module full_adder(a,
+                  b,
+                  c,
+                  sum,
+                  carry);
+
+  input a,b,c;
+  output sum, carry;	     
+
+
+  wire w1, w2, w3;
+
+
+  half_adder HA1(.a(a), .b(b), .sum(w1), .carry(w2));
+  half_adder HA2(.a(w1), .b(c), .sum(sum), .carry(w3));
+
+  or or1(carry, w2, w3);
+
+
+endmodule
+
